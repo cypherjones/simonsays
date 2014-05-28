321c178174304567c711bd0626a2a16bb183755d
aj <ajjonez@icloud.com>
2014-05-27 21:08:25 -0500

    more slugs, and more

--- app/models/category.rb
+++ app/models/category.rb
@@ -9,9 +9,32 @@ class Category <ActiveRecord::Base
   def to_param
     self.slug
   end
+  
+  def generate_slug!
+    the_slug = to_slug(self.name)
+    cat = Category.find_by slug: the_slug
+    count = 2
+    while cat && cat != self
+      the_slug = append_suffix(the_slug, count)
+      cat = Category.find_by slug: the_slug
+      count += 1
+    end    
+    self.slug = the_slug.downcase    
+  end
 
-  def generate_slug
-    self.slug = self.name.gsub(" ", "-").downcase
+  def append_suffix(str, count)
+    if str.split('-').last.to_i != 0
+      return str.split('-').slice(0...-1).join('-') + "-" + count.to_s
+    else
+      return str + "-" + count.to_s
+    end
+  end
+
+  def to_slug(name)
+    str = name.strip
+    str.gsub! /\s*[^A-Za-z0-9]\s*/, '-'
+    str.gsub! /-+/,"-"
+    str
   end
 
 end
